cc39ef1a910b368fd4e73057a8d877f642aab973
Villoro <villoro7@gmail.com>
2019-04-21 20:09:09 +0200

    [NEW] fetch code operator

--- src/dag_expensor.py
+++ src/dag_expensor.py
@@ -4,26 +4,37 @@
 
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.operators import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 from expensor import process
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2019, 4, 19),
-    'email': ['villoro7@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2019, 4, 19),
+    "email": ["villoro7@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-dag = DAG(
-    'expensor', default_args=default_args, schedule_interval=timedelta(days=1)
-)
+dag = DAG("expensor", default_args=default_args, schedule_interval=timedelta(days=1))
 
+
+fetch_code = """
+git fetch
+git checkout master
+git pull origin master
+"""
+
+expensor_fetch_code = BashOperator(
+    task_id="expensor_fetch_code", depends_on_past=False, bash_command=fetch_code, dag=dag
+)
 
 expensor_operator = PythonOperator(
     task_id="expensor_do_all", provide_context=True, python_callable=process.do, dag=dag
 )
+
+# expensor_operator.set_upstream(expensor_fetch_code)
