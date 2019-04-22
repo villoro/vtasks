1993fc5c88ec8b442a4c3b3c4c01bd7b943f5804
Villoro <villoro7@gmail.com>
2019-04-22 12:14:32 +0200

    [REF] drop git fetch operator

--- src/dag_expensor.py
+++ src/dag_expensor.py
@@ -4,11 +4,9 @@
 
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 from expensor import process
-from expensor.config import PATH_ROOT
 
 default_args = {
     "owner": "airflow",
@@ -23,20 +21,6 @@ default_args = {
 
 dag = DAG("expensor", default_args=default_args, schedule_interval=timedelta(days=1))
 
-
-fetch_code = f"""
-cd {PATH_ROOT}
-git fetch
-git checkout master
-git pull origin master
-"""
-
-expensor_fetch_code = BashOperator(
-    task_id="expensor_fetch_code", depends_on_past=False, bash_command=fetch_code, dag=dag
-)
-
 expensor_operator = PythonOperator(
     task_id="expensor_do_all", provide_context=True, python_callable=process.do, dag=dag
 )
-
-# expensor_operator.set_upstream(expensor_fetch_code)
