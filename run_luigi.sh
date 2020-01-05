6af45f55b4471631ca311e64a0c5d91591bf67a9
Ubuntu <ubuntu@ip-172-31-20-162.eu-west-2.compute.internal>
2020-01-05 10:50:19 +0000

    [NEW] add run_luigi.sh

---
+++ run_luigi.sh
@@ -0,0 +1,22 @@
+# Add ssh keys
+eval `keychain --agents ssh --eval github_ssh`
+
+# Go to desired path
+cd /home/ubuntu/villoro_tasks/
+
+# Git fetch and checkout
+git fetch
+git checkout master
+git pull origin master
+
+# Activate virtual environment
+source venv/vtasks/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run luigi
+python src/master.py
+
+# Deactivate virtual environment
+deactivate
