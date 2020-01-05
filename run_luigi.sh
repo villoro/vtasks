0d5c218de371231ece320c4a45c8b16f918382d8
Villoro <villoro7@gmail.com>
2020-01-05 12:25:59 +0100

    [FIX] reorder tasks in run_luigi.sh

--- run_luigi.sh
+++ run_luigi.sh
@@ -4,15 +4,15 @@ eval `keychain --agents ssh --eval github_ssh`
 # Go to desired path
 cd /home/ubuntu/villoro_tasks/
 
+# Activate virtual environment
+source /home/ubuntu/venv/vtasks/bin/activate
+python src/slackbot.py Activate virtual env
+
 # Git fetch and checkout
 git fetch
 git checkout master
 git pull origin master
 python src/slackbot.py Git fetch and pull
-
-# Activate virtual environment
-source /home/ubuntu/venv/vtasks/bin/activate
-python src/slackbot.py Activate virtual env
 
 # Install requirements
 pip install -r requirements.txt
