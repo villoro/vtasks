02301a765ec7888e3fa0fe1e3364d92f6e10ba02
Villoro <villoro7@gmail.com>
2020-01-05 12:15:06 +0100

    [NEW] Slack messages at all steps

--- run_luigi.sh
+++ run_luigi.sh
@@ -8,12 +8,15 @@ cd /home/ubuntu/villoro_tasks/
 git fetch
 git checkout master
 git pull origin master
+python src/slackbot.py Git fetch and pull
 
 # Activate virtual environment
 source /home/ubuntu/venv/vtasks/bin/activate
+python src/slackbot.py Activate virtual env
 
 # Install requirements
 pip install -r requirements.txt
+python src/slackbot.py Install requirements
 
 # Run luigi
 python src/master.py
