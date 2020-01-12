98cc28d4f727f74b3240ab048d846af7d9e691d3
Villoro <villoro7@gmail.com>
2020-01-12 15:50:01 +0100

    [REF] move code form expensor to global_utilities new package + clean unused secrets

---
+++ src/global_utilities/__init__.py
@@ -0,0 +1,2 @@
+from .secrets import get_secret, cipher
+from . import gu_dropbox
---
+++ src/global_utilities/gu_dropbox.py
@@ -0,0 +1,55 @@
+"""
+    Global utilities
+"""
+
+import io
+
+import dropbox
+import pandas as pd
+
+from .secrets import get_secret
+
+
+def get_dbx_connector(key):
+    """
+        Retrive a dropbox connector.
+
+        Args:
+            key:    name of the secret with the dropbox token
+    """
+
+    return dropbox.Dropbox(get_secret(key))
+
+
+def read_excel(dbx, filename, **kwa):
+    """
+        Read an excel from dropbox as a pandas dataframe
+
+        Args:
+            dbx:
+            filename:   name of the excel file
+            **kwa:      keyworded arguments for the pd.read_excel inner function
+    """
+
+    _, res = dbx.files_download(filename)
+    return pd.read_excel(io.BytesIO(res.content), **kwa)
+
+
+def write_excel(dbx, df, filename):
+    """
+        Write an excel to dropbox from a pandas dataframe
+
+        Args:
+            dbx:
+            filename:   name of the excel file
+    """
+
+    output = io.BytesIO()
+
+    writer = pd.ExcelWriter(output)
+    df.to_excel(writer)
+
+    writer.save()
+    output.seek(0)
+
+    dbx.files_upload(output.getvalue(), filename, mode=dropbox.files.WriteMode.overwrite)
