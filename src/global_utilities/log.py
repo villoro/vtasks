3c9fcf420d7edad62427675fc21d6bedd2912d5a
Villoro <villoro7@gmail.com>
2020-01-28 20:35:21 +0100

    [NEW] add stdout in loguru

--- src/global_utilities/log.py
+++ src/global_utilities/log.py
@@ -2,20 +2,20 @@
     Log utilities using loguru
 """
 
+import sys
 from datetime import date
 
 from loguru import logger as log
 
 from config import PATH_ROOT
 
-# fmt: off
+
 CONFIG = {
     "handlers": [
-    	{"sink": f"{PATH_ROOT}logs/{date.today():%Y_%m_%d}.log", "level": "INFO"}
+        {"sink": sys.stdout, "level": "INFO"},
+        {"sink": f"{PATH_ROOT}logs/{date.today():%Y_%m_%d}.log", "level": "INFO"},
     ]
 }
-# fmt: om
 
 log.configure(**CONFIG)
-
 log.enable("vtasks")
