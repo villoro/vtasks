cdf0acc70f59f1c2bb35ab6ede0f71114bc9a9dc
Arnau Villoro <arnau@villoro.com>
2020-11-09 21:44:33 +0100

    set hourly ETL

--- src/heroku.py
+++ src/heroku.py
@@ -1,23 +1,17 @@
-from datetime import date
+import os
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-
 from global_utilities.log import log
-from master import flow
 
 
 schedule = BlockingScheduler()
 
 
-@schedule.scheduled_job("interval", minutes=1)
+@schedule.scheduled_job("interval", hours=1)
 def schedule_flow():
-    """ Schedule flow """
-
-    log.info("Starting vtasks")
-    flow.run(mdate=date.today())
-
-    log.info("End of vtasks")
+    os.system("poetry run python src/master.py")
 
 
 if __name__ == "__main__":
+	log.info("Scheduling vtasks")
     schedule.start()
