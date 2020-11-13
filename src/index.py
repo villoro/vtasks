d4386ef6f4c0d875500e8d9aada585067aafc5a5
Arnau Villoro <arnau@villoro.com>
2020-11-13 18:51:51 +0100

    no gevent

--- src/index.py
+++ src/index.py
@@ -1,13 +1,8 @@
-import gevent
-
 from flask import Flask
 from flask import request
 
-from gevent import monkey
 from global_utilities.secrets import get_secret
 from master import run_etl
-
-monkey.patch_all()
 
 
 app = Flask(__name__)
@@ -16,11 +11,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     if request.headers.get("token") == get_secret("FLASK_PASSWORD"):
-
-        # Schedule job
-        gevent.spawn(run_etl)
-
-        return "ETL scheduled"
+        run_etl()
+        return "vtasks done"
 
     return "Invalid token"
 
