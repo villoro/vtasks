fd9083dcc7c3a7034852de700b768343bb0df04b
Arnau Villoro <arnau@villoro.com>
2020-11-13 17:24:21 +0100

    use headers instead of password

--- src/index.py
+++ src/index.py
@@ -1,24 +1,19 @@
 from flask import Flask
-from flask_httpauth import HTTPDigestAuth
+from flask import request
+
 
 from global_utilities.secrets import get_secret
 
 app = Flask(__name__)
 
-# Needed for digesting
-app.config["SECRET_KEY"] = get_secret("FLASK_SECRET")
-auth = HTTPDigestAuth()
-
-
-@auth.get_password
-def get_password(user):
-    return get_secret("FLASK_PASSWORD")
-
 
 @app.route("/")
-@auth.login_required
 def index():
-    return "Hello, {}!".format(auth.username())
+
+    if request.headers.get("token") == get_secret("FLASK_PASSWORD"):
+        return "Hello!"
+
+    return "Invalid token"
 
 
 if __name__ == "__main__":
