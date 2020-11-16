c382db3b59537ca2ead9e61cfd94c8ab1dc5f690
Arnau Villoro <arnau@villoro.com>
2020-11-16 08:42:47 +0100

    F/heroku (#90)
    * add scheduled task
    
    * use a specific python version
    
    * updating lock
    
    * update python minor version + schedule real ETL
    
    * set hourly ETL
    
    * use a webapp for tasks
    
    * change password
    
    * use headers instead of password
    
    * Async run etl
    
    * no gevent
    
    * single execution + no parallel execution
    
    * working as global variable
    
    * clean flask webapp
    
    * set a dummy procfile
    
    * change to worker
    
    * no procfile
    
    * allow different config for PRO

--- src/master.py
+++ src/master.py
@@ -1,4 +1,6 @@
+from argparse import ArgumentParser
 from datetime import date
+
 from prefect import Flow
 from prefect import Parameter
 from prefect.utilities import logging
@@ -15,19 +17,42 @@ logging.get_logger = lambda x: log
 
 with Flow("do_all") as flow:
     mdate = Parameter("mdate")
+    pro = Parameter("pro")
 
     # Reports part
     df_trans = money_lover(mdate, export_data=True)
-    reports(mdate, df_trans=df_trans)
+    reports(mdate, df_trans=df_trans, pro=pro)
 
     # Flights part
     flights(mdate)
     merge_flights_history(mdate)
 
 
-if __name__ == "__main__":
+def detect_env():
+    """ Detect if it is PRO environment """
+
+    parser = ArgumentParser()
+    parser.add_argument("--pro", help="Wether it is PRO or not (DEV)", default=False, type=bool)
+
+    args = parser.parse_args()
+
+    return args.pro
+
+
+def run_etl():
+    """ Run the ETL for today """
+
+    pro = detect_env()
+
+    if pro:
+        log.info("Working on PRO")
+    else:
+        log.info("Working on DEV")
 
     log.info("Starting vtasks")
-    flow.run(mdate=date.today())
-
+    flow.run(mdate=date.today(), pro=pro)
     log.info("End of vtasks")
+
+
+if __name__ == "__main__":
+    run_etl()
--- src/reports/process.py
+++ src/reports/process.py
@@ -25,23 +25,28 @@ def create_one_report(mdate):
 
 
 @task
-def reports(mdate, df_trans, n_jobs=NUM_OF_JOBS_DEFAULT):
+def reports(mdate, df_trans, pro, n_jobs=NUM_OF_JOBS_DEFAULT):
 
     # TODO: use df_trans input argument instead of reading it from dropbox
 
     mdate = pd.to_datetime(mdate)
     all_dates = pd.date_range(start=MIN_DATE, end=mdate, freq="MS")
 
-    # Do n_jobs in parallel
-    if n_jobs > 1:
-        log.info(f"Doing reports in parallel with {n_jobs} jobs")
-
-        with Pool(n_jobs) as p:
-            p.map(create_one_report, all_dates)
-
-    # No parallelization
-    else:
-        log.info(f"Doing reports without parallelization")
-
-        for x in all_dates.tolist():
+    if pro:
+        log.info(f"Doing reports without parallelization for last 2 months (PRO)")
+        for x in all_dates.tolist()[-2:]:
             create_one_report(x)
+
+    else:
+        # Do n_jobs in parallel (PRO is not powerful enough)
+        if n_jobs > 1:
+            log.info(f"Doing reports in parallel with {n_jobs} jobs")
+
+            with Pool(n_jobs) as p:
+                p.map(create_one_report, all_dates)
+
+        # No parallelization
+        else:
+            log.info(f"Doing reports without parallelization")
+            for x in all_dates.tolist():
+                create_one_report(x)
