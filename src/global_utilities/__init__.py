16ae6c23e1ffdb7c62110b15129b4337a8d5fbf4
Arnau Villoro <arnau@villoro.com>
2020-11-18 09:17:03 +0100

    Copy logs to dropbox

--- src/global_utilities/__init__.py
+++ src/global_utilities/__init__.py
@@ -1,6 +1,7 @@
 from . import dropbox
 
 from .log import log
+from .log import log_path
 from .secrets import cipher
 from .secrets import get_secret
 from .uos import get_path
--- src/global_utilities/log.py
+++ src/global_utilities/log.py
@@ -11,13 +11,12 @@ from loguru import logger as log
 from .uos import get_path
 
 
+log_path = f"logs/{date.today():%Y_%m}/{date.today():%Y_%m_%d}.log"
+
 CONFIG = {
     "handlers": [
         {"sink": sys.stdout, "level": "INFO"},
-        {
-            "sink": get_path(f"logs/{date.today():%Y_%m}/{date.today():%Y_%m_%d}.log"),
-            "level": "INFO",
-        },
+        {"sink": get_path(log_path), "level": "INFO",},
     ]
 }
 
