decd9b64f7291162d3660cee64c1909df4766764
Arnau Villoro <arnau@villoro.com>
2020-12-29 10:28:11 +0100

    refactor report template

--- src/expensor/create_report.py
+++ src/expensor/create_report.py
@@ -27,6 +27,15 @@ def main(mdate=datetime.now(), data=None):
     # Add title
     data["mdate"] = f"{mdate:%Y_%m}"
     data["title"] = f"{mdate:%Y_%m} Expensor"
+    data["sections"] = {
+        "evolution": "fa-chart-line",
+        "comparison": "fa-poll",
+        "pies": "fa-chart-pie",
+        "liquid": "fa-tint",
+        "investments": "fa-wallet",
+        "fire": "fa-fire-alt",
+        "sankey": "fa-stream",
+    }
 
     # Create report
     report = u.render_jinja_template("expensor.html", data)
