be11fe2602bfdc8075860b99a33777d560d4eb76
Arnau Villoro <arnau@villoro.com>
2021-01-29 17:09:03 +0100

    more useful functions

--- src/backups/functions.py
+++ src/backups/functions.py
@@ -1,15 +1,32 @@
 import re
 
 from datetime import date
+from datetime import timedelta
 
 from utils import get_vdropbox
+from utils import log
 
 YEAR = f"{date.today():%Y}"
-MONTH = f"{date.today():%Y_%m}"
 DAY = f"{date.today():%Y_%m_%d}"
 
 
+def get_update_at(vdp, filename):
+    """ Get the date when a file was updated """
+
+    metadata = vdp.dbx.files_get_metadata(filename)
+    return metadata.client_modified.date()
+
+
+def updated_yesterday(vdp, filename):
+    """ True if the file has been updated after the start of yesterday """
+
+    updated_at = get_update_at(vdp, filename)
+
+    return updated_at > date.today() - timedelta(1)
+
+
 def get_files_to_backup(vdp, uri):
+    """ Get a path and a list of files form a regex """
 
     # Extract path and regex
     path = uri.split("/")
@@ -19,3 +36,20 @@ def get_files_to_backup(vdp, uri):
     filenames = [x for x in vdp.ls(path) if re.search(regex, x)]
 
     return path, filenames
+
+
+def backup(uri):
+    """ Backs up some files """
+
+    vdp = get_vdropbox()
+
+    path, filenames = get_files_to_backup(vdp, uri)
+
+    # Backup all files
+    for filename in filenames:
+        origin = f"{path}/{filename}"
+        dest = f"{path}/{YEAR}/{DAY} {filename}"
+
+        log.info(f"Backing up '{origin}'")
+
+        vdp.dbx.files_copy(origin, dest)
