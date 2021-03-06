6d5e7350182df14fd1f67917903bf4315a6f9eb1
Arnau Villoro <arnau@villoro.com>
2021-03-06 20:20:29 +0100

    refactor ing code

--- src/ing.py
+++ src/ing.py
@@ -1,14 +1,15 @@
-import datetime
 import hashlib
 import json
 import requests
 import uuid
 
+from base64 import b64decode
+from base64 import b64encode
+from datetime import datetime
+
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
-from base64 import b64decode
-from base64 import b64encode
 
 
 class INGAPI:
@@ -27,76 +28,93 @@ class INGAPI:
         self.tls_cert = tls_cert
         self.tls_key = tls_key
 
-    def calculate_digest(self, payload):
-        payload_digest = hashlib.sha256()
-        payload_digest.update(payload.encode())
-        return b64encode(payload_digest.digest()).decode()
+        # Set an empty token until it's requested
+        self.token = None
+
+    def digest_body(self, text):
+        """ digest a text with SHA256 """
+
+        digest = hashlib.sha256()
+        digest.update(text.encode())
+
+        return b64encode(digest.digest()).decode()
 
     def sign(self, stringToSign):
 
-        with open(self.file_key, "r") as mykey:
-            private_key = RSA.importKey(mykey.read())
-            signer = PKCS1_v1_5.new(private_key)
-            digest = SHA256.new()
-            digest.update(stringToSign.encode())
-            signingString = signer.sign(digest)
-            signingString = b64encode(signingString).decode()
-            return signingString
+        with open(self.file_key, "r") as file:
+            data = file.read()
 
-    def calculate_signature(self, httpMethod, endpoint, reqDate, digest, reqId):
+        private_key = RSA.importKey(data)
+        signer = PKCS1_v1_5.new(private_key)
+
+        digest = SHA256.new()
+        digest.update(stringToSign.encode())
+
+        signingString = signer.sign(digest)
+        signingString = b64encode(signingString).decode()
+
+        return signingString
+
+    def calculate_signature(self, method, endpoint, mdate, digest, reqId):
         stringToSign = (
-            f"(request-target): {httpMethod} {endpoint}\n"
-            + f"date: {reqDate}\n"
+            f"(request-target): {method} {endpoint}\n"
+            + f"date: {mdate}\n"
             + f"digest: SHA-256={digest}\n"
             + f"x-ing-reqid: {reqId}"
         )
         return self.sign(stringToSign)
 
-    def query(self, httpMethod, endpoint, body="", token=None):
+    def query(self, method, endpoint, body="", token=None):
 
         reqId = str(uuid.uuid4())
-        reqDate = datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
+        mdate = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
         cert = (self.tls_cert, self.tls_key)
-        digest = self.calculate_digest(body)
+        digest = self.digest_body(body)
 
-        signature = self.calculate_signature(httpMethod.lower(), endpoint, reqDate, digest, reqId)
+        signature = self.calculate_signature(method.lower(), endpoint, mdate, digest, reqId)
 
         headers = {
-            "Date": reqDate,
+            "Date": mdate,
             "Digest": f"SHA-256={digest}",
             "X-ING-ReqID": reqId,
-            "Authorization": (
-                f'Signature keyId="{self.client_id}"'
-                + ',algorithm="rsa-sha256"'
-                + ',headers="(request-target) date digest x-ing-reqid"'
-                + f',signature="{signature}"'
-            ),
         }
+
+        signature_details = (
+            f'keyId="{self.client_id}"',
+            'algorithm="rsa-sha256"',
+            'headers="(request-target) date digest x-ing-reqid"',
+            f'signature="{signature}"',
+        )
+
         if token:
             headers.update(
                 {
                     "Authorization": f"Bearer {token}",
-                    "Signature": (
-                        f'keyId="{self.client_id}"'
-                        + ',algorithm="rsa-sha256"'
-                        + ',headers="(request-target) date digest x-ing-reqid"'
-                        + f',signature="{signature}"'
-                    ),
+                    "Signature": ",".join(signature_details),
                     "Accept": "application/json",
                 }
             )
         else:
-            headers.update({"Content-Type": "application/x-www-form-urlencoded"})
+            headers.update(
+                {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Authorization": "Signature " + ",".join(signature_details),
+                }
+            )
 
-        return requests.request(
-            httpMethod.upper(), self.host + endpoint, headers=headers, data=body, cert=cert
+        result = requests.request(
+            method.upper(), self.host + endpoint, headers=headers, data=body, cert=cert
         )
+
+        result.raise_for_status()
+
+        return result
 
     def get_token(self):
         res = self.query(
-            httpMethod="POST",
+            method="POST",
             endpoint="/oauth2/token",
             body="grant_type=client_credentials&scope=greetings%3Aview",
         )
-        res.raise_for_status()
-        return res.json()["access_token"]
+        self.token = res.json()["access_token"]
+        return token
