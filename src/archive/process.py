d204db17956b60396aedf9905a454991d3f0adc2
Arnau Villoro <arnau@villoro.com>
2021-03-19 19:33:20 +0100

    Allow to archive zip files (#124)
    * Allow to archive zip files
    
    * Add missing imports
    
    * Import get_secret

--- src/archive/process.py
+++ src/archive/process.py
@@ -1,15 +1,19 @@
+from datetime import date
+from datetime import timedelta
+
 from prefect import task
 
 from utils import get_files_from_regex
 from utils import get_path
+from utils import get_secret
 from utils import get_vdropbox
 from utils import log
 from utils import read_yaml
 from utils import timeit
 
 
-FILE_REPLACES = get_path("src/archive/replaces.yaml")
-REPLACES = read_yaml(FILE_REPLACES)
+PATH_OPERATIONS = get_path("src/archive/operations.yaml")
+OPERATIONS = read_yaml(PATH_OPERATIONS)
 
 
 def rename_files(vdp, path, regex, output):
@@ -26,11 +30,38 @@ def rename_files(vdp, path, regex, output):
         vdp.delete(origin)
 
 
+def extract_files(vdp, path, regex, output, pwd, kwargs):
+    """ Extract files based on regexs """
+
+    # Evaluate as python expresions
+    kwargs = {key: eval(val) for key, val in kwargs.items()}
+
+    for path, file, _ in get_files_from_regex(vdp, path, regex):
+
+        origin = f"{path}/{file}"
+        dest = f"{path}/{output.format(**kwargs)}"
+
+        log.info(f"Extracting '{origin}'")
+
+        # Extract from zip
+        data = vdp.read_zip(origin, pwd=get_secret(pwd).encode())
+
+        # Export as a file
+        vdp.write_file(data, dest, as_binary=True)
+
+        vdp.delete(origin)
+
+
 @task
 @timeit
 def archive():
 
     vdp = get_vdropbox()
 
-    for kwargs in REPLACES:
+    # Rename some files
+    for kwargs in OPERATIONS["renames"]:
         rename_files(vdp, **kwargs)
+
+    # Extract some files
+    for kwargs in OPERATIONS["extractions"]:
+        extract_files(vdp, **kwargs)
