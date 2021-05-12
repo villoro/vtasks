773d502bff9963a0aab9519d3d504ba4d6f010b1
Arnau Villoro <arnau@villoro.com>
2021-05-12 19:29:11 +0200

    Basic function for sending slacks

---
+++ src/slack.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+from utils import get_secret
+
+
+def send_slack(text, channel="test", blocks=None):
+
+    assert channel in ["test", "events", "general"]
+
+    webhook = get_secret(f"SLACK_WEBHOOK_{channel.upper()}")
+
+    data = {"text": text}
+
+    if blocks:
+        data["blocks"] = blocks
+
+    res = requests.post(
+        webhook, data=json.dumps(data), headers={"Content-Type": "application/json"}
+    )
+
+    res.raise_for_status()
