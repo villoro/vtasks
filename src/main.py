634cb1943f9e870dd1180c860859e342b656b14a
Arnau Villoro <arnau@villoro.com>
2022-12-14 08:56:58 +0100

    tag with env

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from argparse import ArgumentParser
 from datetime import date
 
 from prefect import flow
+from prefect import tags
 
 from archive import archive
 from backups import backup
@@ -13,22 +15,40 @@ from vbooks import vbooks
 from vprefect import vprefect
 
 
-@flow(name="vtasks")
+def detect_env():
+    """Detect if it is PRO environment"""
+
+    parser = ArgumentParser()
+    parser.add_argument("--pro", help="Wether it is PRO or not (DEV)", default=False, type=bool)
+
+    args = parser.parse_args()
+
+    if args.pro:
+        return "prod"
+    return "dev"
+
+
+ENV = detect_env()
+
+
+@flow(name="vtasks", tags=f"env:{ENV}")
 def main(mdate: date):
-    archive()
-    vbooks()
 
-    _gcal = gcal(mdate)
-    backup(wait_for=[_gcal])
+    with tags(f"env:{ENV}"):
+        archive()
+        vbooks()
 
-    _money_lover = money_lover()
-    _crypto = crypto(mdate)
-    _indexa = indexa(mdate)
+        _gcal = gcal(mdate)
+        backup(wait_for=[_gcal])
 
-    expensor(mdate, wait_for=[_money_lover, _crypto, indexa])
+        _money_lover = money_lover()
+        _crypto = crypto(mdate)
+        _indexa = indexa(mdate)
 
-    vprefect()
+        expensor(mdate, wait_for=[_money_lover, _crypto, indexa])
+
+        vprefect()
 
 
 if __name__ == "__main__":
-    main(date.today())
+    main(mdate=date.today())
