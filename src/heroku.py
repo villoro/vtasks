be5ece96fd9c7aa9b8579f50fa1b1a2d18ae61e6
Arnau Villoro <arnau@villoro.com>
2022-12-15 21:30:57 +0100

    F/fix heroku login (#193)
    * use subprocess to call the run so that heroku recognizes the login
    
    * better handling of the env
    
    * pass env from heroku call

---
+++ src/heroku.py
@@ -0,0 +1,24 @@
+import subprocess
+
+from utils import get_secret
+
+from main import detect_env
+
+PREFECT_LOGIN = "prefect cloud login --key {} --workspace villoro/vtasks"
+VTASKS_RUN = f"python src/main.py --env {detect_env()}"
+
+
+def prefect_login():
+    """ugly way to login in heroku machine"""
+    login = PREFECT_LOGIN.format(get_secret("PREFECT_TOKEN"))
+    subprocess.run(login, shell=True)
+
+
+def run_prefect_externally():
+    """Calling it with subprocess so that prefect recoginzes the login"""
+    subprocess.run(VTASKS_RUN, shell=True)
+
+
+if __name__ == "__main__":
+    prefect_login()
+    run_prefect_externally()
