73f19fe9e1b80f59becca148e2efe2bc3c14f0cf
villoro7 <villoro7@gmail.com>
2022-12-18 10:19:53 +0100

    code for cleaning subs

--- scripts/subs/models.py
+++ scripts/subs/models.py
@@ -11,6 +11,7 @@ class Sub(BaseModel):
     name: str
     good_format: bool = False
     language: str = None
+    smart_language: str = None
 
     @staticmethod
     def strip_extension(text):
@@ -39,14 +40,29 @@ class Sub(BaseModel):
             return language
         return None
 
+    @validator("smart_language", pre=True, always=True)
+    def check_smart_language(cls, v, values):
+        movie_name = cls.strip_extension(values["movie"])
+        sub_name = cls.strip_extension(values["name"])
+
+        sub_name = sub_name[len(movie_name) + 1 :]
+        sub_name = sub_name.strip().lower()
+
+        if sub_name in ["spa", "spanish"]:
+            return "es"
+
+        if sub_name.lower() in ["eng", "english"]:
+            return "en"
+
 
 class Movie(BaseModel):
-    folder: str
+    path: str
     movie: str
     subs: list[Sub]
     year: int = 0
     has_good_subs: bool = None
     quality: int = 0
+    count_subs: int = 0
     # Specific subs
     sub_ca: bool = None
     sub_en: bool = None
@@ -55,7 +71,7 @@ class Movie(BaseModel):
 
     @validator("year", pre=True, always=True)
     def check_language(cls, v, values):
-        out = REGEX_YEAR.search(values["folder"])
+        out = REGEX_YEAR.search(values["path"])
         if out:
             return out.groupdict().get("year")
         return 0
@@ -69,10 +85,14 @@ class Movie(BaseModel):
 
     @validator("quality", pre=True, always=True)
     def check_quality(cls, v, values):
-        out = REGEX_QUALITY.search(values["folder"])
+        out = REGEX_QUALITY.search(values["movie"])
         if out:
             return int(out.groupdict().get("quality"))
         return 0
+
+    @validator("count_subs", pre=True, always=True)
+    def check_num_subs(cls, v, values):
+        return len(values["subs"])
 
     @validator("sub_ca", pre=True, always=True)
     def check_sub_es(cls, v, values):
--- scripts/subs/subs_parser.py
+++ scripts/subs/subs_parser.py
@@ -22,14 +22,45 @@ def process_one(folder):
             subs.append(file)
 
     subs = [Sub(movie=movie, name=x) for x in subs]
-    return Movie(folder=folder, movie=movie, subs=subs)
+    return Movie(path=path, movie=movie, subs=subs)
 
 
 def process_all():
     movies = []
     for folder in tqdm(os.listdir(PATH), desc="movies"):
         movies.append(process_one(folder))
+    return movies
 
+
+def movies_to_df(movies):
     df = pd.DataFrame([x.dict() for x in movies])
     del df["subs"]
     return df
+
+
+def default_subs_to_english(movies):
+    count = 0
+
+    for movie in tqdm(movies, desc="default_subs_to_english"):
+        subs = movie.subs
+        if (len(subs) == 1) and ((sub := subs[0]).language is None):
+            old_path = f"{movie.path}/{sub.name}"
+            new_path = f"{movie.path}/{sub.name[:-4]}.en.srt"
+
+            os.rename(old_path, new_path)
+            count += 1
+    return count
+
+
+def fix_using_smart_language(movies):
+    count = 0
+
+    for movie in tqdm(movies, desc="fix_using_smart_language"):
+        for sub in movie.subs:
+            if (sub.language != sub.smart_language) and (sub.smart_language is not None):
+                old_path = f"{movie.path}/{sub.name}"
+                new_path = f"{movie.path}/{sub.movie}.{sub.smart_language}.srt"
+
+                os.rename(old_path, new_path)
+                count += 1
+    return count
