3a8b37d09a964a34a27777f6fe2a1a8be99188c5
villoro7 <villoro7@gmail.com>
2023-01-28 19:58:54 +0100

    rename to battery

--- src/battery/main.py
+++ src/battery/main.py
@@ -12,6 +12,8 @@ PATH_PHONE = "/Aplicaciones/pixel"
 PATH_CSV = f"{PATH_PHONE}/bmw_history.txt"
 PATH_BATTERY = f"{PATH_PHONE}/battery.parquet"
 
+SEND_ALERT_TASK_NAME = "vtasks.phone.alert"
+
 COL_MAP = {
     9: "time",
     1: "battery",
@@ -27,13 +29,13 @@ COL_MAP = {
 
 def read_bmw_history(vdp):
 
-    # log = get_run_logger()
-    # log.info(f"Reading {PATH_CSV=}")
+    log = get_run_logger()
+    log.info(f"Reading {PATH_CSV=}")
     df = vdp.read_csv(PATH_CSV, header=None)
 
     df = df.rename(columns=COL_MAP)
 
-    # log.info(f"Formating dataframe")
+    log.info(f"Formating dataframe")
     df["time"] = df["time"].apply(lambda x: datetime.fromtimestamp(x / 1000))
     df["battery"] = df["battery"].str.split(" ").str[-1].str[:-1].apply(int)
     df["temperature"] = df["temperature"].str[:-2].apply(float)
@@ -55,3 +57,34 @@ def update_parquet(vdp, df_new, parquet_path=PATH_BATTERY):
         df = df_new.copy()
 
     vdp.write_parquet(df, parquet_path)
+
+
+@task(name="vtasks.phone.extract")
+def extract_battery_data():
+    vdp = u.get_vdropbox()
+    df = read_bmw_history(vdp)
+    update_parquet(vdp, df)
+
+
+@task(name="vtasks.phone.needs_alert")
+def needs_summary(mdate: date):
+    """Creates the report"""
+
+    log = get_run_logger()
+
+    task_name = SEND_ALERT_TASK_NAME
+
+    env = u.detect_env()
+    if env != "prod":
+        log.info(f"Skipping summary since {env=}")
+        return False
+
+    log.info(f"Checking if '{task_name}' has already run today")
+    task_runs = asyncio.run(query_task_runs(name_like=task_name, env=env))
+
+    if task_runs:
+        log.warning("Alert already send")
+        return False
+
+    log.info("Sending alert to extract battery")
+    return True
