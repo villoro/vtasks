55f1b663420db163d1fddc374eaf8e5c5584855c
Arnau Villoro <arnau@villoro.com>
2023-05-20 22:49:46 +0200

    F/fix vprefect (#219)
    * Add sortings to avoid problems
    
    * Iterate to extract all flow_runs and task_runs
    
    * Add sleeps and logs
    
    * More logs
    
    * Allow filters + better handling of default params in query_task_runs
    
    * Filter based on last update
    
    * minimize number of reads to history file

--- src/vprefect/query.py
+++ src/vprefect/query.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from datetime import timezone
+from time import sleep
 
 import asyncio
 import pandas as pd
@@ -9,6 +10,7 @@ from prefect import get_run_logger
 from prefect import task
 from prefect.client import get_client
 from prefect.orion.schemas import filters
+from prefect.orion.schemas import sorting
 
 import utils as u
 
@@ -24,21 +26,74 @@ async def read_flows():
     return await client.read_flows()
 
 
-async def read_flow_runs():
-    """extract flow runs"""
+async def _read_flow_runs(offset, flow_run_filter=None):
+    """extract flow runs with query limits"""
     client = get_client()
-    return await client.read_flow_runs()
+    return await client.read_flow_runs(
+        sort=sorting.FlowRunSort.START_TIME_DESC, flow_run_filter=flow_run_filter, offset=offset
+    )
 
 
-async def read_task_runs(task_run_filter=None):
-    """extract task runs"""
+async def read_flow_runs(flow_run_filter=None, max_queries=100):
+    """extract flow runs iterating to avoid query limits"""
+
+    log = get_run_logger()
+    flow_runs = []
+
+    for x in range(max_queries):
+        log.info(f"    Starting iteration {x+1}/{max_queries}")
+        response = await _read_flow_runs(offset=x * 200, flow_run_filter=flow_run_filter)
+        if not response:
+            break
+        flow_runs += response
+        sleep(0.5)
+
+    log.info(f"All flow_runs ({len(flow_runs)}) extracted in {x} API calls ({max_queries=})")
+    return flow_runs
+
+
+async def query_flow_runs(
+    start_time_min=datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0),
+):
     client = get_client()
-    return await client.read_task_runs(task_run_filter=task_run_filter)
+    filter_params = {}
+
+    if start_time_min:
+        filter_params["start_time"] = filters.FlowRunFilterStartTime(after_=start_time_min)
+
+    flow_run_filter = filters.FlowRunFilter(**filter_params)
+    return await read_flow_runs(flow_run_filter)
+
+
+async def _read_task_runs(offset, task_run_filter=None):
+    """extract task runs with query limits"""
+    client = get_client()
+    return await client.read_task_runs(
+        sort=sorting.TaskRunSort.END_TIME_DESC, task_run_filter=task_run_filter, offset=offset
+    )
+
+
+async def read_task_runs(task_run_filter=None, max_queries=200):
+    """extract task runs iterating to avoid query limits"""
+
+    log = get_run_logger()
+    task_runs = []
+
+    for x in range(max_queries):
+        log.info(f"    Starting iteration {x+1}/{max_queries}")
+        response = await _read_task_runs(offset=x * 200, task_run_filter=task_run_filter)
+        if not response:
+            break
+        task_runs += response
+        sleep(0.5)
+
+    log.info(f"All task_runs ({len(task_runs)}) extracted in {x} API calls ({max_queries=})")
+    return task_runs
 
 
 async def query_task_runs(
-    name_like,
-    env,
+    name_like=None,
+    env=None,
     state_names=["Completed"],
     start_time_min=datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0),
 ):
@@ -69,7 +124,6 @@ def handle_localization(df_in):
     # There has been some problems with timezones,
     # make sure to remove them everywhere
     for col in [c.COL_CREATED, c.COL_EXPORTED_AT, c.COL_START, c.COL_END]:
-
         # Skip columns that are not present in some models
         if col not in df.columns:
             continue
@@ -119,16 +173,30 @@ def deduplicate(df_in):
     return df[~df.index.duplicated(keep="first")]
 
 
-def update_parquet(df_new, parquet_path):
+def get_history(vdp, parquet_path):
+    log = get_run_logger()
+
+    if not vdp.file_exists(parquet_path):
+        return None
+
+    log.info(f"Reading {parquet_path=}")
+    df_history = vdp.read_parquet(parquet_path)
+    return handle_localization(df_history)
+
+
+def get_last_update(df_history):
+    if df_history is None:
+        return None
+    return df_history[c.COL_START].max()
+
+
+def update_parquet(vdp, df_new, df_history, parquet_path):
     log = get_run_logger()
 
     vdp = u.get_vdropbox()
 
-    if vdp.file_exists(parquet_path):
+    if df_history is not None:
         log.info(f"Updating {parquet_path=}")
-        df_history = vdp.read_parquet(parquet_path)
-        df_history = handle_localization(df_history)
-
         df = pd.concat([df_new, df_history])
         df = deduplicate(df)
     else:
@@ -140,7 +208,6 @@ def update_parquet(df_new, parquet_path):
 
 
 def add_flow_name(df_in, flows):
-
     df = df_in.copy()
 
     flow_map = parse_prefect(flows, Flow)[c.COL_NAME].to_dict()
@@ -151,12 +218,20 @@ def add_flow_name(df_in, flows):
 
 @task(name="vtasks.vprefect.flow_runs", retries=3, retry_delay_seconds=5)
 def process_flow_runs():
+    vdp = u.get_vdropbox()
     log = get_run_logger()
+
+    log.info("Querying last_update")
+    df_history = get_history(vdp, c.PATH_FLOW_RUNS)
+    last_update = get_last_update(df_history)
 
     log.info("Querying flows")
     flows = asyncio.run(read_flows())
     log.info("Querying flow_runs")
-    flow_runs = asyncio.run(read_flow_runs())
+    flow_runs = asyncio.run(query_flow_runs(start_time_min=last_update))
+
+    log.info("Removing invalid flow_runs")
+    flow_runs = [x for x in flow_runs if x.start_time is not None]
 
     log.info("Processing flow_runs")
     df_new = parse_prefect(flow_runs, FlowRun)
@@ -168,18 +243,29 @@ def process_flow_runs():
     # Retrive flow_name from flows
     df_new = add_flow_name(df_new, flows)
 
-    update_parquet(df_new, c.PATH_FLOW_RUNS)
+    update_parquet(vdp, df_new, df_history, c.PATH_FLOW_RUNS)
 
 
 @task(name="vtasks.vprefect.task_runs", retries=3, retry_delay_seconds=5)
 def process_task_runs():
+    vdp = u.get_vdropbox()
     log = get_run_logger()
 
+    log.info("Querying last_update")
+    df_history = get_history(vdp, c.PATH_TASK_RUNS)
+    last_update = get_last_update(df_history)
+
     log.info("Querying task_runs")
-    task_runs = asyncio.run(read_task_runs())
+    task_runs = asyncio.run(query_task_runs(start_time_min=last_update, state_names=None))
+
+    log.info("Removing invalid task_runs")
+    task_runs = [x for x in task_runs if x.start_time is not None]
 
     log.info("Processing task_runs")
     df_new = parse_prefect(task_runs, TaskRun)
     df_new = extract_tags(df_new)
 
-    update_parquet(df_new, c.PATH_TASK_RUNS)
+    # Exclude running tags
+    df_new = df_new[df_new[c.COL_STATE] != c.STATE_RUNNING]
+
+    update_parquet(vdp, df_new, df_history, c.PATH_TASK_RUNS)
