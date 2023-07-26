6b63d4d94cf603b98d91788308490f237c45ed9c
Arnau Villoro <arnau@villoro.com>
2023-07-26 11:31:00 +0200

    F/books type (#229)
    * Prepare columns and colors
    
    * Add year by type plot

--- src/vbooks/constants.py
+++ src/vbooks/constants.py
@@ -10,6 +10,7 @@ COL_LANGUAGE = "Language"
 COL_AUTHOR = "Author"
 COL_SOURCE = "Source"
 COL_OWNED = "Owned?"
+COL_TYPE = "Type"
 
 STATUS_OWNED = "Owned"
 STATUS_IN_LIBRARY = "In Library"
@@ -19,14 +20,22 @@ STATUS_NOT_OWNED = "Not Owned"
 # fmt: off
 COLORS = {
     "Total": ("black", 500), "Total_dim": ("grey", 600),
+    # Languages
     "Català": ("blue", 500), "Català_dim": ("blue", 300),
     "Español": ("red", 500), "Español_dim": ("red", 300),
     "English": ("green", 500), "English_dim": ("green", 300),
     "Italiano": ("purple", 500), "Italiano_dim": ("purple", 300),
+    # Types:
+    "Fiction": ("grey", 100),
+    "CP": ("purple", 400),
+    "Finances": ("green", 400),
+    "IT": ("orange", 400),
+    "Other non fiction": ("red", 400),
+    # Ownership
+    STATUS_OWNED: ("green", 500), STATUS_IN_LIBRARY: ("amber", 500), STATUS_NOT_OWNED: ("red", 500),
+    # Others
     "Years": ("grey", 500),
     "Authors": ("blue", 800),
     "Sources": ("deep orange", 600),
-    # Ownership
-    STATUS_OWNED: ("green", 500), STATUS_IN_LIBRARY: ("amber", 500), STATUS_NOT_OWNED: ("red", 500),
 }
 # fmt: on
