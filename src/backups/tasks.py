3a37b3c2d579c3f66a4350acc547893b59e6b3c2
Arnau Villoro <arnau@villoro.com>
2024-01-31 18:50:39 +0100

    Remove a backup task (#254)
    * Remove a backup task
    
    * Remove another task
    
    * fix version

--- src/backups/tasks.py
+++ src/backups/tasks.py
@@ -7,8 +7,6 @@ COPY_TASKS = [
 
 BACKUP_TASKS = [
     BackupTask(path="/Aplicaciones/FreeFileSync", regex=r"[\w\s]+\.ffs_gui"),
-    BackupTask(path="/Aplicaciones/gcalendar", regex=r"calendar.parquet"),
     BackupTask(path="/Aplicaciones/KeePass", regex=r"[\w\s]+\.kdbx"),
-    BackupTask(path="/Aplicaciones/vtasks", regex=r"[\w_]+.parquet"),
     BackupTask(path="/Casa/KeePass", regex=r"[\w\s]+\.kdbx"),
 ]
