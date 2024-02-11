27231b7f80dd92996b5c93752aace9e96df6f87d
Arnau Villoro <arnau@villoro.com>
2024-02-11 21:17:33 +0100

    Add some copies and backups + fix path (#256)
    * Add some copies and backups + fix path
    
    * Fix version
    
    * missing comas

--- src/backups/tasks.py
+++ src/backups/tasks.py
@@ -2,11 +2,17 @@ from .models import BackupTask
 from .models import CopyTask
 
 COPY_TASKS = [
-    CopyTask(origin="/Aplicaciones/KeePass/General.kdbx", destination="/Casa/KeePass/Arnau.kdbx")
+    CopyTask(
+        origin="/Aplicaciones/KeePass/General.kdbx", destination="/Groups/Casa/KeePass/Arnau.kdbx"
+    ),
+    CopyTask(
+        origin="/Aplicaciones/KeePass/General.kdbx", destination="/Groups/KeePass Cocco/Arnau.kdbx"
+    ),
 ]
 
 BACKUP_TASKS = [
     BackupTask(path="/Aplicaciones/FreeFileSync", regex=r"[\w\s]+\.ffs_gui"),
     BackupTask(path="/Aplicaciones/KeePass", regex=r"[\w\s]+\.kdbx"),
-    BackupTask(path="/Casa/KeePass", regex=r"[\w\s]+\.kdbx"),
+    BackupTask(path="/Groups/Casa/KeePass", regex=r"[\w\s]+\.kdbx"),
+    BackupTask(path="/Groups/KeePass Cocco", regex=r"[\w\s]+\.kdbx"),
 ]
