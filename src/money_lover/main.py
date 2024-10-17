c8aa341c410e7c1d598b2147b10c2de3fec2c6e0
Arnau Villoro <arnau@villoro.com>
2024-10-17 21:04:35 +0200

    Fix black

--- src/money_lover/main.py
+++ src/money_lover/main.py
@@ -98,9 +98,8 @@ def process_transtactions(vdp, df_in):
     # Handle FraVi
     df[c.COL_TOTAL_AMOUNT] = df[c.COL_AMOUNT]
     mask = df[c.COL_ACCOUNT] == c.ACCOUNT_FRAVI
-    df.loc[mask, c.COL_AMOUNT] = (
-        df[mask, c.COL_AMOUNT] * 0.82
-    )  # TODO: use dynamic percent
+    # TODO: use dynamic percent
+    df.loc[mask, c.COL_AMOUNT] = df[mask, c.COL_AMOUNT] * 0.82
 
     # Export
     vdp.write_excel(df[c.COLS_DF_TRANS], c.FILE_TRANSACTIONS)
