22c583d6202f5f60e44dda080407ca8ab4590180
Arnau Villoro <arnau@villoro.com>
2025-01-08 19:03:19 +0100

    Improve log handling

--- src/common/logs.py
+++ src/common/logs.py
@@ -1,20 +1,29 @@
+import logging
 import sys
 
-from loguru import logger as loguru_logger
 from prefect import get_run_logger
 from prefect.exceptions import MissingContextError
 
-LOGURU_LEVEL = "DEBUG"
+LOG_LEVEL = "DEBUG"
 
-loguru_logger.configure(handlers=[{"sink": sys.stdout, "level": LOGURU_LEVEL}])
-loguru_logger.enable("vtasks")
+# Try importing loguru, fallback to standard logging if not available
+try:
+    from loguru import logger
+
+    logger.configure(handlers=[{"sink": sys.stdout, "level": LOG_LEVEL}])
+    logger.enable("vtasks")
+
+except ImportError:
+    # Set up basic logging as a fallback
+    logging.basicConfig(level=LOG_LEVEL)
+    logger = logging.getLogger("fallback_logger")
 
 
 def get_logger(force_loguru=False):
     if force_loguru:
-        return loguru_logger
+        return logger
 
     try:
         return get_run_logger()
     except MissingContextError:
-        return loguru_logger
+        return logger
