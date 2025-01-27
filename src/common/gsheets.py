72fc6e50ce8f4992650ff884aa8e2e9bc8cc98c7
Arnau Villoro <arnau@villoro.com>
2025-01-27 20:03:59 +0100

    Code for uploading a range

--- src/common/gsheets.py
+++ src/common/gsheets.py
@@ -69,7 +69,7 @@ def _get_gdrive_sheet(doc, sheet, max_tries=5):
     return _fetch_sheet()
 
 
-def read_gdrive_sheet(doc, sheet, max_tries=5):
+def read_gdrive_sheet(doc, sheet, with_index=False, max_tries=5):
     """Read data from a Google Drive sheet and return it as a DataFrame."""
     logger = get_logger()
     gsheet = _get_gdrive_sheet(doc, sheet, max_tries)
@@ -83,7 +83,12 @@ def read_gdrive_sheet(doc, sheet, max_tries=5):
     data = _fetch_data()
 
     logger.info(f"Data read from 'gsheet://{doc}.{sheet}'")
-    return pd.DataFrame(data)
+    df = pd.DataFrame(data)
+
+    if with_index:
+        return df.set_index(df.columns[0])
+
+    return df
 
 
 def update_cell(doc, sheet, cell, value, max_tries=5):
@@ -101,3 +106,76 @@ def update_cell(doc, sheet, cell, value, max_tries=5):
     _update_cell()
 
     logger.info(f"{cell=} successfully updated in 'gsheet://{doc}.{sheet}'")
+
+
+def _get_coordinates(df):
+    """Get gdrive coordinates as a pandas dataframe"""
+
+    df_index = df.copy()
+
+    def index_to_letter(x):
+        """Get column letter (Chr(65) = 'A')"""
+        return chr(65 + x + 1)
+
+    n_rows = df_index.shape[0]
+    numbers = pd.Series([str(x + 2) for x in range(n_rows)], index=df_index.index)
+
+    for i, col in enumerate(df_index.columns):
+        df_index[col] = index_to_letter(i) + numbers
+
+    return df_index
+
+
+def _get_range_to_update(df, mfilter, columns):
+    """Gets the range that needs to be updated"""
+
+    # If no columns are passed, update them all
+    if columns is None:
+        columns = df.columns.tolist()
+
+    # Make sure columns is a list
+    if not isinstance(columns, list):
+        columns = [columns]
+
+    # Extract range from coordinates and filter
+    coordinates = _get_coordinates(df).loc[mfilter, columns]
+
+    if isinstance(coordinates, pd.Series):
+        return f"{coordinates.iloc[0]}:{coordinates.iloc[-1]}"
+
+    return f"{coordinates.iloc[0, 0]}:{coordinates.iloc[-1, -1]}"
+
+
+def _get_values_to_update(df, mfilter, columns):
+    # Filter data to be updated
+    values = df.loc[mfilter, columns].values.tolist()
+
+    # Make sure that values is a list of lists
+    if not isinstance(values[0], list):
+        values = [values]
+
+    return values
+
+
+def df_to_gspread(doc, sheet, df, mfilter, cols=None, max_tries=5):
+    """Update a google spreadsheet based on a pandas dataframe row"""
+
+    logger = get_logger()
+
+    gsheet = _get_gdrive_sheet(doc, sheet, max_tries)
+
+    logger.info(f"Preparing update for 'gsheet://{doc}.{sheet}' ({mfilter=}, {cols=})")
+
+    mrange = _get_range_to_update(df, mfilter, cols)
+    values = _get_values_to_update(df, mfilter, cols)
+
+    # Update values in gspreadsheet
+    logger.info(f"Updating 'gsheet://{doc}.{sheet}' ({mrange=})")
+
+    @retry_on_exception(max_tries=max_tries)
+    def _update_cells():
+        gsheet.update(mrange, values)
+
+    _update_cells()
+
+    logger.info(f"{mrange=} successfully updated in 'gsheet://{doc}.{sheet}'")
