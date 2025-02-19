56c4b480a5078da242b773c09e6256146617d603
Arnau Villoro <arnau@villoro.com>
2025-02-19 08:22:13 +0100

    Declare the jobs as a dict

--- src/schedules/hourly.py
+++ src/schedules/hourly.py
@@ -1,6 +1,6 @@
 from prefect import flow
+from prefect import states
 from prefect import tags
-from prefect.states import Failed
 
 from src.common.logs import get_logger
 from src.jobs.backups.main import backup_all
@@ -12,53 +12,52 @@ from src.jobs.gsheets.export_tables import export_gsheets_tables
 from src.jobs.indexa.main import indexa_all
 from src.vdbt.python.run import run_dbt
 
+FLOW_NAME = "vtasks.hourly"
+
+JOBS = {
+    "maintain": [backup_all],
+    "updates": [crypto, indexa_all],
+    "export": [
+        export_dropbox_tables,
+        export_money_lover,
+        export_all_gcal,
+        export_gsheets_tables,
+    ],
+}
+
 
 def run_flows(flows, flow_name):
     """Execute a list of flows, collect states, and fail if any flow fails."""
 
     logger = get_logger()
 
-    states = {flow.__name__: flow(return_state=True) for flow in flows}
+    results = {mflow.__name__: mflow(return_state=True) for mflow in flows}
 
     # Detect failed flows
-    failures = [name for name, state in states.items() if isinstance(state, Failed)]
+    failures = [
+        name
+        for name, state in results.items()
+        if isinstance(state, states.State) and state.is_failed()
+    ]
 
     if failures:
         msg = f"❌ {flow_name} failed ({len(failures)} flows failed, {failures=})."
         logger.error(msg)
-        return Failed(message=msg)
+        return states.Failed(message=msg)
 
     logger.info(f"✅ {flow_name} completed successfully!")
     return states
 
 
-@flow(name="vtasks.hourly.maintain")
-def maintain():
-    flows = [backup_all]
-    return run_flows(flows, "maintain")
-
-
-@flow(name="vtasks.hourly.updates")
-def updates():
-    flows = [crypto, indexa_all]
-    return run_flows(flows, "updates")
-
-
-@flow(name="vtasks.hourly.export")
-def export():
-    flows = [
-        export_dropbox_tables,
-        export_money_lover,
-        export_all_gcal,
-        export_gsheets_tables,
-    ]
-    return run_flows(flows, "export")
-
-
-@flow(name="vtasks.hourly")
+@flow(name=FLOW_NAME)
 def hourly():
     with tags("env:pro"):
-        return run_flows([maintain, updates, export, run_dbt], "hourly")
+        flows = [
+            flow(name=f"{FLOW_NAME}.{name}")(flows, name)
+            for name, flows in JOBS.items()
+        ]
+        flows.append(run_flows([run_dbt], "dbt"))
+        return run_flows(flows, "hourly")
 
 
 if __name__ == "__main__":
