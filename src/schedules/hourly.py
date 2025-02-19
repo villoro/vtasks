52c50af171b50f0ba2cfcf8307f8d49b5ae92039
Arnau Villoro <arnau@villoro.com>
2025-02-19 08:55:49 +0100

    F/improve failures handling (#279)
    * Improve failures handling
    
    * Simplify code
    
    * Declare the jobs as a dict
    
    * Go back to manually listing jobs
    
    * Increase version

--- src/schedules/hourly.py
+++ src/schedules/hourly.py
@@ -1,5 +1,8 @@
 from prefect import flow
+from prefect import states
+from prefect import tags
 
+from src.common.logs import get_logger
 from src.jobs.backups.main import backup_all
 from src.jobs.crypto.main import crypto
 from src.jobs.dropbox.export_tables import export_dropbox_tables
@@ -9,32 +12,67 @@ from src.jobs.gsheets.export_tables import export_gsheets_tables
 from src.jobs.indexa.main import indexa_all
 from src.vdbt.python.run import run_dbt
 
+FLOW_NAME = "vtasks.hourly"
 
-@flow(name="vtasks.hourly.maintain")
+JOBS = {
+    "maintain": [backup_all],
+    "updates": [crypto, indexa_all],
+    "export": [
+        export_dropbox_tables,
+        export_money_lover,
+        export_all_gcal,
+        export_gsheets_tables,
+    ],
+}
+
+
+def is_failure(state):
+    """Checks if it's a failed state"""
+    return isinstance(state, states.State) and state.is_failed()
+
+
+def run_flows(flows, flow_name):
+    """Execute a list of flows, collect states, and fail if any flow fails."""
+
+    logger = get_logger()
+
+    results = {mflow.__name__: mflow(return_state=True) for mflow in flows}
+
+    # Detect failed flows
+    failures = [name for name, state in results.items() if is_failure(state)]
+
+    if failures:
+        msg = f"❌ {flow_name} failed ({len(failures)} {failures=})."
+        logger.error(msg)
+        return states.Failed(message=msg)
+
+    logger.info(f"✅ {flow_name} completed successfully!")
+    return states
+
+
+@flow(name=f"{FLOW_NAME}.maintain")
 def maintain():
-    backup_all()
+    name = "maintain"
+    return run_flows(JOBS[name], name)
 
 
-@flow(name="vtasks.hourly.updates")
+@flow(name=f"{FLOW_NAME}.updates")
 def updates():
-    crypto()
-    indexa_all()
+    name = "updates"
+    return run_flows(JOBS[name], name)
 
 
-@flow(name="vtasks.hourly.export")
+@flow(name=f"{FLOW_NAME}.export")
 def export():
-    export_dropbox_tables()
-    export_money_lover()
-    export_all_gcal()
-    export_gsheets_tables()
+    name = "export"
+    return run_flows(JOBS[name], name)
 
 
-@flow(name="vtasks.hourly")
+@flow(name=FLOW_NAME)
 def hourly():
-    maintain()
-    updates()
-    export()
-    run_dbt()
+    with tags("env:pro"):
+        flows = [maintain, updates, export, run_dbt]
+        return run_flows(flows, "hourly")
 
 
 if __name__ == "__main__":
