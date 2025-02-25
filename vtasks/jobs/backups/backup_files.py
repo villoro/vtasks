1f182aa32afb813c14e59809085e6d309f7c8267
Arnau Villoro <arnau@villoro.com>
2025-02-25 20:15:41 +0100

    Migrate to UV (#286)
    * Migrate to uv
    
    * Add packages
    
    * Add more packages
    
    * Add setuptools
    
    * Update vcrypto code
    
    * Remove and upgrade dependencies
    
    * rename src to vdropbox
    
    * Fix deployments
    
    * Add missing config
    
    * Update vdropbox + fix imports
    
    * Update vcrypto
    
    * Some work
    
    * Export requirements
    
    * Pre-commit hook for updating requirements
    
    * Increase version
    
    * Missing version update
    
    * Fix logger in vdropbox
    
    * Remove poetry from integration tests
    
    * Use vhooks
    
    * Use specific version
    
    * Fix path

---
+++ vtasks/jobs/backups/backup_files.py
@@ -0,0 +1,61 @@
+from datetime import date
+from datetime import timedelta
+
+from prefect import flow
+from prefect import task
+
+from vtasks.common.dropbox import get_vdropbox
+from vtasks.common.dropbox import scan_folder_and_subfolders_by_regex
+from vtasks.common.logs import get_logger
+from vtasks.jobs.backups.tasks import BACKUP_TASKS
+
+YEAR = f"{date.today():%Y}"
+DAY = f"{date.today():%Y_%m_%d}"
+FLOW_NAME = "backups.backup_files"
+
+
+def get_update_at(vdp, filename):
+    """Get the date when a file was updated"""
+
+    metadata = vdp.dbx.files_get_metadata(filename)
+    return metadata.client_modified.date()
+
+
+def one_backup(vdp, path, regex):
+    """Back up a list of files from a folder"""
+
+    logger = get_logger()
+
+    # Backup all files
+    for path, filename in scan_folder_and_subfolders_by_regex(path, regex, vdp=vdp):
+        origin = f"{path}/{filename}"
+        logger.debug(f"Trying to backup '{origin}")
+        updated_at = get_update_at(vdp, origin)
+
+        if updated_at >= date.today() - timedelta(1):
+            dest = f"{path}/Backups/{YEAR}/{updated_at:%Y_%m_%d} {filename}"
+
+            if not vdp.file_exists(dest):
+                logger.info(f"Copying '{origin}' to '{dest}'")
+                vdp.dbx.files_copy(origin, dest)
+
+            else:
+                logger.debug(f"File '{origin}' has already been backed up")
+
+        else:
+            logger.debug(f"Skipping '{origin}' since has not been updated")
+
+
+@flow(name=FLOW_NAME)
+def backup_files():
+    """Back up all files from URIS"""
+
+    vdp = get_vdropbox()
+
+    for backup_task in BACKUP_TASKS:
+        name = f"{FLOW_NAME}.{backup_task.path[1:].replace('/', '_')}"
+        task(name=name)(one_backup)(vdp, **backup_task.dict())
+
+
+if __name__ == "__main__":
+    backup_files()
---
+++ vtasks/jobs/crypto/main.py
@@ -0,0 +1,106 @@
+from datetime import date
+
+import cryptocompare
+import pandas as pd
+from prefect import flow
+from prefect import task
+
+from vtasks.common import gsheets
+
+
+# crypto_data spreadsheet info
+SPREADSHEET_CRYPTO = "crypto_data"
+SHEET_PRICES = "prices"
+SHEET_VALUE = "value"
+SHEET_VOL_KRAKEN = "vol_kraken"
+SHEET_SUMMARY = "summary"
+
+# expensor_data spreadsheet info
+SPREADSHEET_EXPENSOR = "expensor_data"
+SHEET_WORTH = "worth_m"
+COL_CRYPTO = "crypto"
+
+
+# Crypto id: cell in summary
+MAIN_CRYPTOS = {"BTC": "B9", "ETH": "C9"}
+
+FLOW_NAME = "crypto"
+
+
+def get_market_cap(cryptos, order_magnitude=10**9):
+    """Get market capitalization of the asked coins"""
+
+    data = cryptocompare.get_price(cryptos, full=True)
+
+    return {
+        key: values["EUR"]["MKTCAP"] / order_magnitude
+        for key, values in data["RAW"].items()
+    }
+
+
+@task(name=f"{FLOW_NAME}.update_market_cap")
+def update_market_cap():
+    """Update market capitalization in google spreadsheet"""
+    volumes = get_market_cap(list(MAIN_CRYPTOS))
+
+    for crypto, cell in MAIN_CRYPTOS.items():
+        gsheets.update_cell(SPREADSHEET_CRYPTO, SHEET_SUMMARY, cell, volumes[crypto])
+
+
+def get_crypto_prices(cryptos):
+    """Get latest prices of a list of cryptos"""
+
+    # Query cryptos
+    data = cryptocompare.get_price([*cryptos])
+
+    # Create a dict with prices
+    return {i: x["EUR"] for i, x in data.items()}
+
+
+@task(name=f"{FLOW_NAME}.update_crypto_prices")
+def update_crypto_prices(mfilter):
+    """Update latest cryptos prices"""
+
+    df = gsheets.read_gdrive_sheet(SPREADSHEET_CRYPTO, SHEET_PRICES, with_index=True)
+
+    # Update prices
+    values = get_crypto_prices(df.columns)
+    df.loc[mfilter] = pd.Series(values)
+
+    # Update gspreadsheet
+    gsheets.df_to_gspread(SPREADSHEET_CRYPTO, SHEET_PRICES, df, mfilter)
+
+
+def to_number(x):
+    return float(x.replace(".", "").replace(",", ".").replace(" €", ""))
+
+
+@task(name=f"{FLOW_NAME}.update_expensor")
+def update_expensor(mfilter):
+    """Update expensor cryptos worth based on crypto values"""
+
+    # Get worths
+    df = gsheets.read_gdrive_sheet(SPREADSHEET_EXPENSOR, SHEET_WORTH, with_index=True)
+
+    # Get worth of actual month
+    prices = gsheets.read_gdrive_sheet(SPREADSHEET_CRYPTO, SHEET_VALUE, with_index=True)
+
+    # Update kraken value
+    value = prices.at[mfilter, "Total"]
+    df.at[mfilter, COL_CRYPTO] = to_number(value)
+
+    gsheets.df_to_gspread(SPREADSHEET_EXPENSOR, SHEET_WORTH, df, mfilter, COL_CRYPTO)
+
+
+@flow(name=FLOW_NAME)
+def crypto():
+    mfilter = date.today().strftime("%Y/%m")
+
+    update_market_cap()
+
+    update_crypto_prices(mfilter)
+    update_expensor(mfilter)
+
+
+if __name__ == "__main__":
+    crypto()
