a84a51b0c049be2210ad0465c70b06fb2347e6b7
Arnau Villoro <arnau@villoro.com>
2025-02-28 19:28:09 +0100

    Simplify query

--- vtasks/vdbt/models/marts/gcal/marts_gcal__working_days.sql
+++ vtasks/vdbt/models/marts/gcal/marts_gcal__working_days.sql
@@ -12,32 +12,22 @@ WITH filtered_data AS (
 with_moving_averages AS (
     SELECT
         start_day,
-        duration_hours AS total_hours,
-        AVG(duration_hours) OVER (
+        ROUND(duration_hours, 2) AS total_hours,
+        ROUND(AVG(duration_hours) OVER win_7d, 2) AS moving_avg_7_days,
+        ROUND(AVG(duration_hours) OVER win_30d, 2) AS moving_avg_30_days
+    FROM filtered_data
+    WINDOW 
+        win_7d AS (
             PARTITION BY calendar_name
             ORDER BY start_day
             ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-        ) AS moving_avg_7_days,
-        AVG(duration_hours) OVER (
+        ),
+        win_30d AS (
             PARTITION BY calendar_name
             ORDER BY start_day
             ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
-        ) AS moving_avg_30_days
-    FROM filtered_data
-    ORDER BY 1 DESC
-),
-
-values_rounded AS (
-    SELECT
-        -------- dims
-        start_day,
-
-        -------- measures
-        round(total_hours, 2) AS total_hours,
-        round(moving_avg_7_days, 2) AS moving_avg_7_days,
-        round(moving_avg_30_days, 2) AS moving_avg_30_days
-    FROM with_moving_averages
+        )
 )
 
 SELECT *
-FROM values_rounded
+FROM with_moving_averages
