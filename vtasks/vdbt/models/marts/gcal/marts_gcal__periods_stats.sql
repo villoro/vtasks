3fe58177d9d9d9d24907e485923437b592faf006
Arnau Villoro <arnau@villoro.com>
2025-02-28 19:44:14 +0100

    Add also total days

--- vtasks/vdbt/models/marts/gcal/marts_gcal__periods_stats.sql
+++ vtasks/vdbt/models/marts/gcal/marts_gcal__periods_stats.sql
@@ -1,11 +1,23 @@
-{% set time_windows = {
-    'current_week': "start_day >= DATE_TRUNC('week', CURRENT_DATE)",
-    'current_month': "start_day >= DATE_TRUNC('month', CURRENT_DATE)",
-    'current_year': "start_day >= DATE_TRUNC('year', CURRENT_DATE)",
-    'last_week': "start_day >= DATE_TRUNC('week', CURRENT_DATE) - INTERVAL 7 DAY AND start_day < DATE_TRUNC('week', CURRENT_DATE)",
-    'last_month': "start_day >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL 1 MONTH AND start_day < DATE_TRUNC('month', CURRENT_DATE)",
-    'last_year': "start_day >= DATE_TRUNC('year', CURRENT_DATE) - INTERVAL 1 YEAR AND start_day < DATE_TRUNC('year', CURRENT_DATE)"
+{# Define base dates #}
+{% set base_dates = {
+    'week': "DATE_TRUNC('week', CURRENT_DATE)",
+    'month': "DATE_TRUNC('month', CURRENT_DATE)",
+    'year': "DATE_TRUNC('year', CURRENT_DATE)"
 } %}
+
+{# Define current time windows #}
+{% set current_windows = [
+    ('current_week', base_dates['week']),
+    ('current_month', base_dates['month']),
+    ('current_year', base_dates['year'])
+] %}
+
+{# Define last time windows #}
+{% set last_windows = [
+    ('last_week', base_dates['week'] + " - INTERVAL 7 DAY", base_dates['week']),
+    ('last_month', base_dates['month'] + " - INTERVAL 1 MONTH", base_dates['month']),
+    ('last_year', base_dates['year'] + " - INTERVAL 1 YEAR", base_dates['year'])
+] %}
 
 WITH daily_stats AS (
     SELECT *
@@ -13,14 +25,30 @@ WITH daily_stats AS (
 ),
 
 time_aggregations AS (
-    {# Loop through each time window to generate a UNION ALL query #}
-    {% for window, condition in time_windows.items() %}
+    {# Loop for current periods #}
+    {% for window_name, start_date in current_windows %}
     SELECT 
         calendar_name,
-        '{{ window }}' AS time_window,
-        ROUND(SUM(duration_hours), 2) AS total_duration
+        '{{ window_name }}' AS time_window,
+        SUM(duration_hours) AS total_duration,
+        DATE_DIFF('day', {{ start_date }}, CURRENT_DATE) + 1 AS num_days
     FROM daily_stats
-    WHERE {{ condition }}
+    WHERE start_day >= {{ start_date }}
+    GROUP BY calendar_name
+    {% if not loop.last %} UNION ALL {% endif %}
+    {% endfor %}
+
+    UNION ALL
+
+    {# Loop for last periods #}
+    {% for window_name, start_date, end_date in last_windows %}
+    SELECT 
+        calendar_name,
+        '{{ window_name }}' AS time_window,
+        SUM(duration_hours) AS total_duration,
+        DATE_DIFF('day', {{ start_date }}, {{ end_date }}) AS num_days
+    FROM daily_stats
+    WHERE start_day >= {{ start_date }} AND start_day < {{ end_date }}
     GROUP BY calendar_name
     {% if not loop.last %} UNION ALL {% endif %}
     {% endfor %}
