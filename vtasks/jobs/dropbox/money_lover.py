0abeb8b8119a2df7ef40e29920e5a61592ef2913
Arnau Villoro <arnau@villoro.com>
2025-03-21 16:27:55 +0100

    F/money lover fixes (#295)
    * Export file
    
    * Refactor file
    
    * Delete all but last file
    
    * Patch version

--- vtasks/jobs/dropbox/money_lover.py
+++ vtasks/jobs/dropbox/money_lover.py
@@ -1,3 +1,5 @@
+import re
+
 from prefect import flow
 from prefect import task
 
@@ -22,37 +24,66 @@ def get_files(vdp):
     return [x[1] for x in matches]
 
 
-@task(name=f"{FLOW_NAME}.export_last_file")
-def export_last_file(vdp, files):
+@task(name=f"{FLOW_NAME}.read_file")
+def read_file(vdp, file_path):
+    logger = get_logger()
+    sep = dropbox.infer_separator(file_path, vdp=vdp)
+
+    logger.info(f"Reading {file_path=}")
+    return vdp.read_csv(file_path, index_col=0, sep=sep)
+
+
+@task(name=f"{FLOW_NAME}.backup_csv")
+def backup_csv(vdp, df, filename):
     logger = get_logger()
 
-    last_file = f"{PATH_ML}/{files[-1]}"
-    sep = dropbox.infer_separator(last_file, vdp=vdp)
+    match = re.match(REGEX_MONEY_LOVER, filename)
+    if not match:
+        logger.error(
+            f"Wrong money lover file {filename=} doesn't follow {REGEX_MONEY_LOVER=}"
+        )
+        return False
 
-    logger.info(f"Reading {last_file=}")
-    df = vdp.read_csv(last_file, index_col=0, sep=sep)
+    file_date = match.groupdict().get("date")
+    path_parquet = f"{PATH_ML}/{file_date[:4]}/{file_date}.parquet"
+    logger.info(f"Exporting {path_parquet=}")
+    vdp.write_parquet(df, path_parquet)
+    return True
 
-    df["_source"] = f"dropbox:/{last_file}"
-    write_df(df, SCHEMA_OUT, TABLE_OUT, mode="append")
+
+@task(name=f"{FLOW_NAME}.export_last_file")
+def export_last_file(df, file_path):
+    df["_source"] = f"dropbox:/{file_path}"
+    write_df(df, SCHEMA_OUT, TABLE_OUT)
 
 
 @task(name=f"{FLOW_NAME}.remove_files")
 def remove_files(vdp, files):
     logger = get_logger()
 
-    logger.info(f"Removing {len(files)} money lover files")
-    for file in files:
+    logger.info(f"Removing {len(files) - 1} money lover files")
+    for file in files[:-1]:
         vdp.delete(f"{PATH_ML}/{file}")
 
 
 @flow(name=f"{FLOW_NAME}")
 def export_money_lover():
+    logger = get_logger()
     vdp = dropbox.get_vdropbox()
 
-    files = get_files(vdp)
-    export_last_file(vdp, files)
-    # TODO: Enable this once migrated
-    # remove_files(vdp, files)
+    if not (files := get_files(vdp)):
+        logger.info("There are no files to process")
+        return False
+
+    filename = files[-1]
+    file_path = f"{PATH_ML}/{filename}"
+
+    df = read_file(vdp, file_path)
+    backup_csv(vdp, df, filename)
+    export_last_file(df, file_path)
+
+    remove_files(vdp, files)
+    return True
 
 
 if __name__ == "__main__":
