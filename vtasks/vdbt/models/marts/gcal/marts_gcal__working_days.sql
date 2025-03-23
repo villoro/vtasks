957168ab31b8b586e6f2b14b2aa4b377f4898c76
Arnau Villoro <arnau@villoro.com>
2025-03-23 12:01:35 +0100

    Add p10 and p90 intervals to gcal

--- vtasks/vdbt/models/marts/gcal/marts_gcal__working_days.sql
+++ vtasks/vdbt/models/marts/gcal/marts_gcal__working_days.sql
@@ -1,34 +1,49 @@
-{% set work_calendar = '11_Paid work' %}
+{% set paid_work_calendar = '11_Paid work' %}
+{% set work_calendar = '12_Work' %}
 {% set min_working_hours = 3 %}
 
 WITH filtered_data AS (
     SELECT *
     FROM {{ ref('marts_gcal__daily_stats') }}
-    WHERE
-        calendar_name = '{{ work_calendar }}'
+    WHERE (
+        calendar_name = '{{ paid_work_calendar }}'
         AND duration_hours > {{ min_working_hours }}
+    ) OR calendar_name = '{{ work_calendar }}'
 ),
 
 with_moving_averages AS (
     SELECT
         start_day,
-        ROUND(duration_hours, 2) AS total_hours,
-        ROUND(AVG(duration_hours) OVER win_7d, 2) AS moving_avg_7_days,
-        ROUND(AVG(duration_hours) OVER win_30d, 2) AS moving_avg_30_days
+        duration_hours AS total_hours,
+        AVG(duration_hours) OVER win_7d AS mavg_7d,
+        AVG(duration_hours) OVER win_30d AS mavg_30d,
+        quantile_cont(duration_hours, 0.10) OVER win_30d AS p10_30d,
+        quantile_cont(duration_hours, 0.90) OVER win_30d AS p90_30d
     FROM filtered_data
     WINDOW 
         win_7d AS (
             PARTITION BY calendar_name
             ORDER BY start_day
-            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+            ROWS BETWEEN 3 PRECEDING AND 3 FOLLOWING  
         ),
         win_30d AS (
             PARTITION BY calendar_name
             ORDER BY start_day
-            ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+            ROWS BETWEEN 15 PRECEDING AND 15 FOLLOWING  
         )
     ORDER BY 1 DESC
+),
+
+rounded AS (
+    SELECT
+        start_day,
+        ROUND(total_hours, 2) AS total_hours,
+        ROUND(mavg_7d, 2) AS mavg_7d,
+        ROUND(mavg_30d, 2) AS mavg_30d,
+        ROUND(p10_30d, 2) AS p10_30d,
+        ROUND(p90_30d, 2) AS p90_30d
+    FROM with_moving_averages
 )
 
 SELECT *
-FROM with_moving_averages
+FROM rounded
