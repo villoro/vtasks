b6d53d2515a689b5219654cabc48c781296d85be
Arnau Villoro <arnau@villoro.com>
2025-03-26 19:27:38 +0100

    Improve fra model

--- vtasks/vdbt/models/marts/gcal/marts_gcal__fra.sql
+++ vtasks/vdbt/models/marts/gcal/marts_gcal__fra.sql
@@ -1,9 +1,22 @@
 {% set min_date = '2025-01-01' %}
 
-WITH source AS (
+WITH events AS (
     SELECT *
-    FROM {{ ref('marts_gcal__daily_stats') }}
-    WHERE NOT is_personal AND start_day > DATE '{{ min_date }}'
+    FROM {{ ref('stg_gcal__events') }}
+    WHERE NOT is_personal AND NOT is_whole_day_event
+),
+
+daily_stats AS (
+    SELECT
+        -------- dims
+        started_at :: date AS start_day,
+
+        -------- measures
+        round(sum(duration_hours), 2) AS duration_hours
+    FROM events
+    WHERE started_at :: date > DATE '{{ min_date }}'
+    GROUP BY ALL
+    ORDER BY ALL
 ),
 
 final AS (
@@ -12,7 +25,7 @@ final AS (
         duration_hours,
         DATE_TRUNC('week', start_day) AS week,
         STRFTIME(DATE_TRUNC('month', start_day), '%Y-%m') AS month
-    FROM source
+    FROM daily_stats
     ORDER BY 1
 )
 
