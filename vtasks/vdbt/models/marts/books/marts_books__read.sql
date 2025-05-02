55f855d03803de7843449849a0cbe5ce9e70fd41
Arnau Villoro <arnau@villoro.com>
2025-05-02 13:25:58 +0200

    F/reading time (#306)
    * Add reading_time
    
    * Patch version

--- vtasks/vdbt/models/marts/books/marts_books__read.sql
+++ vtasks/vdbt/models/marts/books/marts_books__read.sql
@@ -1,7 +1,43 @@
-WITH source AS (
+{% set cal_books = '34_Books' %}
+
+WITH books AS (
+    SELECT * FROM {{ ref('stg_gsheets__books_read') }}
+),
+
+raw_calendar AS (
     SELECT *
-    FROM {{ ref('stg_gsheets__books_read') }}
+    FROM {{ ref('stg_gcal__events') }}
+    WHERE calendar_name = '{{ cal_books }}'
+),
+
+calendar AS (
+    SELECT
+        lower(trim(title)) AS book,
+        EXTRACT('year' FROM started_at) AS read_year,
+        round(sum(duration_hours), 2) AS total_hours
+    FROM raw_calendar
+    GROUP BY 1, 2
+),
+
+joined AS (
+    SELECT
+        -------- info
+        books.title,
+        books.author,
+        books.language,
+        books.type,
+
+        -------- time related
+        books.read_date,
+
+        -------- measures
+        books.num_pages,
+        SUM(calendar.total_hours) AS total_hours
+    FROM books
+    LEFT JOIN calendar
+        ON lower(trim(books.title)) = calendar.book
+        AND ABS(EXTRACT('year' FROM books.read_date) - calendar.read_year) < 2
+    GROUP BY ALL
 )
 
-SELECT *
-FROM source
+SELECT * FROM joined
