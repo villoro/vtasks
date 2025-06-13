f2779a1cae95d61df609a08b9faee0e71348758e
Arnau Villoro <arnau@villoro.com>
2025-06-13 19:14:45 +0200

    Fix/fra work overlapings (#310)
    * Fix fra work overlapings
    
    * Patch version

--- vtasks/jobs/gsheets/fra_work.py
+++ vtasks/jobs/gsheets/fra_work.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from prefect import flow
 
 from vtasks.common.duck import read_query
@@ -9,6 +10,12 @@ SHEET = "data"
 QUERY = "SELECT * FROM dbt._marts__gcal.marts_gcal__fra"
 
 
+def get_empty_df(df_in, margin=20):
+    df_empty = pd.DataFrame(columns=df_in.columns, index=range(df_in.shape[0] + margin))
+    df_empty[:] = ""
+    return df_empty
+
+
 @flow(name="gsheets.update_fra_work")
 def update_fra_work():
     df = read_query(QUERY, filename="dbt")
@@ -16,6 +23,8 @@ def update_fra_work():
     for col in ["start_day", "week"]:
         df[col] = df[col].dt.strftime("%Y-%m-%d")
 
+    df_empty = get_empty_df(df)
+    df_to_gspread(SPREADSHEET, SHEET, df_empty)
     df_to_gspread(SPREADSHEET, SHEET, df)
 
 
