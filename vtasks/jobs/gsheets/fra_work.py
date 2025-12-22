14c175987228b0114d19c492ae71ba46c6b4ffb8
Arnau Villoro <arnau@villoro.com>
2025-12-22 13:19:47 +0100

    Fix export

--- vtasks/jobs/gsheets/fra_work.py
+++ vtasks/jobs/gsheets/fra_work.py
@@ -7,7 +7,15 @@ from vtasks.common.gsheets import df_to_gspread
 SPREADSHEET = "registro_lavoro"
 SHEET = "data"
 
-QUERY = "SELECT * FROM villoro._marts__gcal.marts_gcal__fra"
+QUERY = """
+SELECT
+    start_day AS day,
+    title AS concept,
+    duration_hours AS total_hours,
+    week,
+    month
+FROM villoro._marts__gcal.marts_gcal__fra
+"""
 
 
 def get_empty_df(df_in, margin=20):
@@ -20,7 +28,7 @@ def get_empty_df(df_in, margin=20):
 def update_fra_work():
     df = read_query(QUERY, use_md=True)
 
-    for col in ["start_day", "week"]:
+    for col in ["day", "week"]:
         df[col] = df[col].dt.strftime("%Y-%m-%d")
 
     df_empty = get_empty_df(df)
