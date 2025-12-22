9e552dcb5963842e73a2fa833087f320c48f5861
Arnau Villoro <arnau@villoro.com>
2025-12-22 09:44:02 +0100

    Add home stats

---
+++ vtasks/vdbt/models/core/expensor/internal/int_expensor__home_stats.sql
@@ -0,0 +1,47 @@
+WITH source AS (
+    SELECT * FROM {{ ref('stg_gsheets__mortgage') }}
+),
+
+renamed AS (
+    SELECT
+        -------- pks
+        month AS change_date,
+
+        -------- measures
+        ownership_personal AS home_ownership,
+        debt_personal AS home_debt,
+        home_value_personal AS home_value,
+
+        -------- metadata
+        _source,
+        _exported_at,
+        _n_updates
+    FROM source
+),
+
+unpivoted AS (
+    UNPIVOT renamed
+    ON home_ownership, home_debt, home_value
+    INTO
+    NAME account_name
+    VALUE value_eur
+),
+
+final AS (
+    SELECT
+        -------- pks
+        change_date,
+        'home' AS account_type,
+        account_name,
+
+        -------- measures
+        value_eur,
+
+        -------- metadata
+        _source,
+        _exported_at,
+        _n_updates
+    FROM mortgage
+)
+
+SELECT * FROM final
