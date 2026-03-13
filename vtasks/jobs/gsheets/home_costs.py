11326914891b36fbed4e193ba89a102dac58f424
Arnau Villoro <arnau@villoro.com>
2026-03-13 18:21:06 +0100

    Reuse code

--- vtasks/jobs/gsheets/home_costs.py
+++ vtasks/jobs/gsheets/home_costs.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from prefect import flow
 
 from vtasks.common.duck import read_query
@@ -18,20 +17,11 @@ WHERE lower(account) = 'home'
 """
 
 
-def get_empty_df(df_in, margin=20):
-    df_empty = pd.DataFrame(columns=df_in.columns, index=range(df_in.shape[0] + margin))
-    df_empty[:] = ""
-    return df_empty
-
-
 @flow(name="gsheets.update_home_costs")
 def update_home_costs():
     df = read_query(QUERY, use_md=True)
     df["data"] = df["data"].dt.strftime("%Y-%m-%d")
-
-    df_empty = get_empty_df(df)
-    df_to_gspread(SPREADSHEET, SHEET, df_empty, start_column=1)
-    df_to_gspread(SPREADSHEET, SHEET, df, start_column=1)
+    df_to_gspread(SPREADSHEET, SHEET, df, start_column=0)
 
 
 if __name__ == "__main__":
